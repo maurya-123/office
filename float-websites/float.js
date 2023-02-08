e9b264f78a3d8089948345e4b3a7aeee302c9025
maurya-123 <124710510+maurya-123@users.noreply.github.com>
2023-02-09 00:42:07 +0530

    ok

---
+++ float-websites/float.js
@@ -0,0 +1,13 @@
+function grow(){
+    var h = document.getElementById("uniq");
+
+    if(h.style.display == "block")
+    {
+      h.style.display = "none";
+    }
+
+    else{
+      h.style.display = "block";
+    }
+
+  }
